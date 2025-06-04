5a200bf309e991af707ab52ee798c31f2365704d
Relph1119 <huruifeng1202@163.com>
2025-06-04 21:12:02 +0800

    :ok_hand: 配置参数，使得内容生成稳定

--- utils/load_model.py
+++ utils/load_model.py
@@ -19,15 +19,17 @@ base_url = os.getenv("BASE_URL")
 client = OpenAI(api_key=api_key, base_url=base_url)
 
 
-def get_completion(prompt, model="deepseek-ai/DeepSeek-V3"):
+def get_completion(prompt, model="Qwen/Qwen2-7B-Instruct"):
     """
     prompt: 对应的提示词
-    model: 调用的模型，默认为 deepseek-ai/Deepseek-V3
+    model: 调用的模型，默认为Qwen/Qwen2-7B-Instruct
     """
     messages = [{"role": "user", "content": prompt}]
     response = client.chat.completions.create(
         model=model,
-        messages=messages
+        messages=messages,
+        temperature=0,
+        seed=0,
     )
     #  0penAI 的 ChatCompletion 接口
     return response.choices[0].message.content
